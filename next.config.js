24a5dbdc30801bd7483eb10bf15e5ce2fd7abdea
LammieJ <118027040+LammieJ@users.noreply.github.com>
2025-02-01 20:37:09 +0000

    fix: update deployment configuration and document required environment variables

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   typescript: {
     ignoreBuildErrors: true
   },
   eslint: {
     ignoreDuringBuilds: true
-  },
-  swcMinify: false,
-  optimizeFonts: false,
-  compress: false
+  }
 }
 
 module.exports = nextConfig
