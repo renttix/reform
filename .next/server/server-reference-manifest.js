6811f4c8237d1816e276c8bd728bcc9e3b4e49eb
LammieJ <118027040+LammieJ@users.noreply.github.com>
2025-02-01 21:25:34 +0000

    fix: Optimize build configuration and remove patch-next script

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"NhZzAwTooKT8UPEFvSAgrN6SzZnWbXMvbeNHslpeiyQ=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Pg5+H5mf0sbZZbrsUDLMmWFd+TDrHVVJ8fZSelhKOi4=\"\n}"
--- next.config.js
+++ next.config.js
@@ -6,7 +6,17 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true
   },
-  experimental: {}
+  poweredByHeader: false,
+  compress: true,
+  generateBuildId: () => 'build',
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2
+  },
+  swcMinify: true,
+  optimizeFonts: true,
+  reactStrictMode: true,
+  output: 'standalone'
 }
 
 module.exports = nextConfig
